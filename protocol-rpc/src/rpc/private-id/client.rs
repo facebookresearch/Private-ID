0aac9d8c373834c6be16291be5ee7478c7538ead
danielmasny <46358615+danielmasny@users.noreply.github.com>
2022-02-28 21:01:00 -0800

    fix issue but haven't renamed or  change step 16 (#28)
    * fix issue but haven't renamed or  change step 16
    
    * Fix bug, make protocol efficient
    
    * Removed commented line
    
    Co-authored-by: Daniel Masny <dmasny@fb.com>
    Co-authored-by: Shubho Sengupta <ssengupta@fb.com>

--- protocol-rpc/src/rpc/private-id/client.rs
+++ protocol-rpc/src/rpc/private-id/client.rs
@@ -287,14 +287,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             _ => panic!("wrong ack"),
         };
 
-    // 12. Get data that partner has but company doesn't
-    let mut s_prime_partner = TPayload::new();
+    // 12. Get data that partner has but company doesn't (send Sp to P)
+    let mut s_partner = TPayload::new();
     let _ = rpc_client::recv(
         ServiceResponse {
             ack: Some(Ack::CalculateSetDiffAck(calculate_set_diff_ack.clone())),
         },
-        "s_prime_partner".to_string(),
-        &mut s_prime_partner,
+        "s_partner".to_string(),
+        &mut s_partner,
         &mut client_context,
     )
     .await?;
@@ -312,10 +312,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await?;
 
     // 14. Encrypt and send back data that partner has company doesn't
-    //     Generates s_double_prime_partner in-place
+    //     Generates s_prime_partner in-place
     let _ = rpc_client::send(
-        partner_protocol.encrypt(s_prime_partner)?,
-        "s_double_prime_partner".to_string(),
+        partner_protocol.encrypt(s_partner)?,
+        "s_prime_partner".to_string(),
         &mut client_context,
     )
     .await?
