fe32011f65368f10c06eb754218a6dfc16a84df2
yajc <jiancaocs@gmail.com>
2022-05-05 15:19:59 -0700

    move tests of load_json to fileio file

--- protocol/tests/fileio.rs
+++ protocol/tests/fileio.rs
@@ -23,32 +23,9 @@ fn test_load_data() {
 
 
     let plain_data = Arc::new(RwLock::new(KeyedCSV::default()));
-    let c = Arc::clone(&plain_data);
-    load_data(plain_data, "./tests/keyed_ints.csv", false);
+    load_data(plain_data.clone(), "./tests/keyed_ints.csv", false);
 
-    let r1 = c.read().unwrap();
+    let r1 = plain_data.read().unwrap();
     assert_eq!(*r1.headers, t.headers);
-    assert_ne!((*r1).records.clone(), t.records);
-}
-
-#[test]
-fn test_load_json() {
-    let j = r#"["a, 1, 10", "b, 2, 20", "c, 3, 30", "d, 4, 40"]"#;
-    let t = KeyedCSV {
-        headers : vec![],
-        records : HashMap::from([
-            (String::from("a"), vec![String::from("1"), String::from("10")]),
-            (String::from("b"), vec![String::from("2"), String::from("20")]),
-            (String::from("c"), vec![String::from("3"), String::from("30")]),
-            (String::from("d"), vec![String::from("4"), String::from("40")]),
-        ]),
-    };
-
-    let plain_data = Arc::new(RwLock::new(KeyedCSV::default()));
-    let c = Arc::clone(&plain_data);
-    load_json(plain_data, j, false);
-
-    let r1 = c.read().unwrap();
-    assert_eq!(*r1.headers, t.headers);
-    assert_ne!((*r1).records.clone(), t.records);
+    assert_eq!((*r1).records.clone(), t.records);
 }
