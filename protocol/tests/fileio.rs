9737f771c3f51be0aca544e841766282e82f3b58
yajc <jiancaocs@gmail.com>
2022-05-09 13:49:24 -0700

    Added test for load_data and load_json function (#42)
    * Added test for load_data function
    
    * fix bug
    
    * nit
    
    * compare hashmap directly
    
    * Add test for load_json
    
    * move tests of load_json to fileio file
    
    Co-authored-by: Jian Cao <jiancao0421@fb.com>

---
+++ protocol/tests/fileio.rs
@@ -0,0 +1,31 @@
+//  Copyright (c) Facebook, Inc. and its affiliates.
+//  SPDX-License-Identifier: Apache-2.0
+
+extern crate protocol;
+use protocol::fileio::*;
+use std::{
+    collections::HashMap,
+    path::Path,
+    sync::{Arc, RwLock},
+};
+
+#[test]
+fn test_load_data() {
+    let t = KeyedCSV {
+        headers : vec![],
+        records : HashMap::from([
+            (String::from("a"), vec![String::from("1"), String::from("10")]),
+            (String::from("b"), vec![String::from("2"), String::from("20")]),
+            (String::from("c"), vec![String::from("3"), String::from("30")]),
+            (String::from("d"), vec![String::from("4"), String::from("40")]),
+        ]),
+    };
+
+
+    let plain_data = Arc::new(RwLock::new(KeyedCSV::default()));
+    load_data(plain_data.clone(), "./tests/keyed_ints.csv", false);
+
+    let r1 = plain_data.read().unwrap();
+    assert_eq!(*r1.headers, t.headers);
+    assert_eq!((*r1).records.clone(), t.records);
+}
