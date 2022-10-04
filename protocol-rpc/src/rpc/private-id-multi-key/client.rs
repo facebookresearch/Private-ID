facebookresearch/Private-ID

commit 1d9d07f68861f813ec65f5be1e5aa7baed51b271

Make PID multi-key protocol metric output path configurable (#43)
Co-authored-by: Jian Cao <jiancao0421@fb.com>

protocol-rpc/src/rpc/private-id-multi-key/server.rs
@@ -52,6 +52,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .short("o")
                 .takes_value(true)
                 .help("Path to output file, output format: private-id, option(key)"),
+            Arg::with_name("metric-path")
+                .long("metric-path")
+                .takes_value(true)
+                .help("Path to metric output file"),
             Arg::with_name("stdout")
                 .long("stdout")
                 .short("u")
@@ -120,6 +124,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     let input_with_headers = matches.is_present("input-with-headers");
     let output_path = matches.value_of("output");
+    let metric_path = matches.value_of("metric-path");
 
     let no_tls = matches.is_present("no-tls");
     let host = matches.value_of("host");
@@ -139,6 +144,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("Input path: {}", input_path);
 
+    let mut metrics_output_path: Option<String> = None;
+    if metric_path.is_some() {
+        metrics_output_path = Some(metric_path.unwrap().to_string());
+        if metrics_output_path.is_none() {
+            metrics_output_path = Some(format!("{}_metrics", output_path.unwrap()));
+        }
+    }
+
     if output_path.is_some() {
         info!("Output path: {}", output_path.unwrap());
     } else {
@@ -146,7 +159,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let service =
-        rpc_server::PrivateIdMultiKeyService::new(&input_path, output_path, input_with_headers);
+        rpc_server::PrivateIdMultiKeyService::new(&input_path, output_path, input_with_headers, metrics_output_path);
 
     let ks = service.killswitch.clone();
     let recv_thread = thread::spawn(move || {

protocol-rpc/src/rpc/private-id-multi-key/rpc_server.rs
@@ -18,7 +18,7 @@ use std::{
     },
 };
 use tonic::{Code, Request, Response, Status, Streaming};
-
+mod metrics;
 use common::{gcs_path::GCSPath, s3_path::S3Path, timer};
 use protocol::private_id_multi_key::{
     company::CompanyPrivateIdMultiKey, traits::CompanyPrivateIdMultiKeyProtocol,
@@ -38,6 +38,8 @@ pub struct PrivateIdMultiKeyService {
     input_path: String,
     output_path: Option<String>,
     input_with_headers: bool,
+    metrics_path: Option<String>,
+    metrics_obj: metrics::Metrics,
     pub killswitch: Arc<AtomicBool>,
 }
 
@@ -46,12 +48,17 @@ impl PrivateIdMultiKeyService {
         input_path: &str,
         output_path: Option<&str>,
         input_with_headers: bool,
+        metrics_path: Option<String>,
     ) -> PrivateIdMultiKeyService {
         PrivateIdMultiKeyService {
             protocol: CompanyPrivateIdMultiKey::new(),
             input_path: String::from(input_path),
             output_path: output_path.map(String::from),
             input_with_headers,
+            metrics_path,
+            metrics_obj: metrics::Metrics::new(
+                "private-id-multi-key".to_string(),
+            ),
             killswitch: Arc::new(AtomicBool::new(false)),
         }
     }
@@ -278,6 +285,9 @@ impl PrivateIdMultiKey for PrivateIdMultiKeyService {
             .extra_label("reveal")
             .build();
         self.protocol.write_company_to_id_map();
+        self.metrics_obj.set_partner_input_size(self.protocol.get_e_partner_size());
+        self.metrics_obj.set_publisher_input_size(self.protocol.get_e_company_size());
+        self.metrics_obj.set_union_file_size(self.protocol.get_id_map_size());
         match &self.output_path {
             Some(p) => {
                 if let Ok(output_path_s3) = S3Path::from_str(p) {
@@ -313,6 +323,27 @@ impl PrivateIdMultiKey for PrivateIdMultiKeyService {
                 }
             }
             None => self.protocol.print_id_map(),
+        }
+        match &self.metrics_path {
+            Some(p) => {
+                if let Ok(metrics_path_s3) = S3Path::from_str(p) {
+                    let s3_tempfile = tempfile::NamedTempFile::new().unwrap();
+                    let (_file, path) = s3_tempfile.keep().unwrap();
+                    let path = path.to_str().expect("Failed to convert path to str");
+                    self.metrics_obj.save_metrics(&String::from(path))
+                        .expect("Failed to metrics to tempfile");
+                    metrics_path_s3
+                        .copy_from_local(&path)
+                        .await
+                        .expect("Failed to write to S3");
+                } else {
+                    self.metrics_obj.save_metrics(p)
+                        .expect("Failed to write to metrics path");
+                }
+            }
+            None => {
+                self.metrics_obj.print_metrics();
+            }
         }
         {
             debug!("Setting up flag for graceful down");

protocol/src/private_id_multi_key/company.rs
@@ -69,6 +69,18 @@ impl CompanyPrivateIdMultiKey {
     pub fn load_data(&self, path: &str, input_with_headers: bool) {
         load_data(self.plaintext.clone(), path, input_with_headers);
     }
+
+    pub fn get_e_company_size(&self) -> usize {
+        self.e_company.read().unwrap().len()
+    }
+
+    pub fn get_e_partner_size(&self) -> usize {
+        self.e_partner.read().unwrap().len()
+    }
+
+    pub fn get_id_map_size(&self) -> usize {
+        self.id_map.read().unwrap().len()
+    }
 }
 
 impl Default for CompanyPrivateIdMultiKey {
@@ -491,5 +503,27 @@ impl CompanyPrivateIdMultiKeyProtocol for CompanyPrivateIdMultiKey {
                 "Unable to write partner view to file".to_string(),
             )),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get_e_company_size() {
+        let b = CompanyPrivateIdMultiKey::new();
+        assert_eq!(b.get_e_company_size(), 0);
+    }
+
+    #[test]
+    fn test_get_e_partner_size() {
+        let b = CompanyPrivateIdMultiKey::new();
+        assert_eq!(b.get_e_partner_size(), 0);
+    }
+
+    #[test]
+    fn test_get_id_map_size() {
+        let b = CompanyPrivateIdMultiKey::new();
+        assert_eq!(b.get_id_map_size(), 0);
     }
 }

commit fed39b0baa4283dca7d81040b14a7bedb19f3402

Add metrics logging to partner (#91)
Summary:
Pull Request resolved: https://github.com/facebookresearch/Private-ID/pull/91

# Context
We are adding metrics logging on multiple reasons.
1. To pass number of splits to Id Combiner as a workaround of [S3 API issue](https://fb.workplace.com/groups/pidmatchingxfn/posts/493743615908631).
2. To use union data size to calculate mutate number of MPC shards as mentioned in [the comment](https://fb.workplace.com/groups/164332244998024/posts/937649537666287/?comment_id=937655107665730&reply_comment_id=937696664328241)

To achieve above, we are enabling metrics logging on both single-key and multi-key PID protocols.

In addition, we found a small bug introduced in [#43](https://github.com/facebookresearch/Private-ID/pull/43) on publisher-side which is not logging metrics to a file when metrics path is not specified. We are fixing it in this diff.

# Description
The way it works is exactly the same as publisher-side metrics logging. We added a new arg `metric-path` in the executables.

1. When metrics path is specified, then metrics would be logged to the specified path.
2. When metrics path is not specified by output path is specified, then metrics would be logged to `{output path}_metrics` file.
3. When output path is not specified and stdout is used, metrics would be logged to stdout.

We are logging three metrics.
- partner_input_size
- publisher_input_size
- union_file_size

Differential Revision: D39320561

fbshipit-source-id: 3b148f5f6b0fe98f00fb6724596c2ba1612f9ba3

protocol-rpc/src/rpc/private-id-multi-key/client.rs
@@ -7,6 +7,7 @@ use clap::App;
 use clap::Arg;
 use clap::ArgGroup;
 use common::gcs_path::GCSPath;
+use common::metrics;
 use common::s3_path::S3Path;
 use common::timer;
 use crypto::prelude::TPayload;
@@ -52,6 +53,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .short("o")
                 .takes_value(true)
                 .help("Path to output file, output format: private-id, option(key)"),
+            Arg::with_name("metric-path")
+                .long("metric-path")
+                .takes_value(true)
+                .help("Path to metric output file"),
             Arg::with_name("stdout")
                 .long("stdout")
                 .short("u")
@@ -140,6 +145,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input_with_headers = matches.is_present("input-with-headers");
     let output_path = matches.value_of("output");
 
+    let metric_path = matches.value_of("metric-path");
+    let metrics = metrics::Metrics::new("private-id-multi-key".to_string());
+    let mut metrics_output_path: Option<String> = None;
+    if let Some(val) = metric_path {
+        metrics_output_path = Some(val.to_string());
+    }
+
     let mut client_context = {
         let no_tls = matches.is_present("no-tls");
         let host_pre = matches.value_of("company");
@@ -167,6 +179,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("Input path: {}", input_path);
     if output_path.is_some() {
         info!("Output path: {}", output_path.unwrap());
+        if metrics_output_path.is_none() {
+            metrics_output_path = Some(format!("{}_metrics", output_path.unwrap()));
+        }
     } else {
         info!("Output view to stdout (first 10 items)");
     }
@@ -181,6 +196,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .load_data(&input_path.to_string(), input_with_headers)
         .unwrap();
     let u_partner = partner_protocol.permute_hash_to_bytes().unwrap();
+    metrics.set_partner_input_size(partner_protocol.get_size());
 
     // 5. Initialize company - this loads company's data and generates its permutation pattern
     let init_ack = match client_context
@@ -226,13 +242,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .try_into()
                 .unwrap(),
         ) as usize;
-
         let offset = u_company
             .drain(data_len..)
             .map(|b| u64::from_le_bytes(b.buffer.as_slice().try_into().unwrap()) as usize)
             .collect::<Vec<_>>();
         u_company.shrink_to_fit();
 
+        // offset is an array of exclusive-inclusive prefix sum - hence number of records
+        // is one less than length
+        metrics.set_publisher_input_size(offset_len - 1);
         assert_eq!(offset_len, offset.len());
         partner_protocol.encrypt_permute(u_company, offset).unwrap()
     };
@@ -352,6 +370,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 16. Create partner's ID spine and print
     partner_protocol.create_id_map(v_partner, s_prime_company);
+    metrics.set_union_file_size(partner_protocol.get_id_map_size());
     match output_path {
         Some(p) => {
             if let Ok(output_path_s3) = S3Path::from_str(p) {
@@ -384,7 +403,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         None => partner_protocol.print_id_map(),
     }
-
+    match &metrics_output_path {
+        Some(p) => {
+            if let Ok(metrics_path_s3) = S3Path::from_str(p) {
+                let s3_tempfile = tempfile::NamedTempFile::new().unwrap();
+                let (_file, path) = s3_tempfile.keep().unwrap();
+                let path = path.to_str().expect("Failed to convert path to str");
+                metrics
+                    .save_metrics(&String::from(path))
+                    .expect("Failed to write metrics to tempfile");
+                metrics_path_s3
+                    .copy_from_local(&path)
+                    .await
+                    .expect("Failed to write to S3");
+            } else {
+                metrics
+                    .save_metrics(p)
+                    .expect("Failed to write to metrics path");
+            }
+        }
+        None => {
+            metrics.print_metrics();
+        }
+    }
     // 17. Create company's ID spine and print
     rpc_client::reveal(&mut client_context).await?;
     global_timer.qps("total time", partner_protocol.get_size());

protocol-rpc/src/rpc/private-id-multi-key/server.rs
@@ -151,13 +151,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut metrics_output_path: Option<String> = None;
     if metric_path.is_some() {
         metrics_output_path = Some(metric_path.unwrap().to_string());
-        if metrics_output_path.is_none() {
-            metrics_output_path = Some(format!("{}_metrics", output_path.unwrap()));
-        }
     }
 
     if output_path.is_some() {
         info!("Output path: {}", output_path.unwrap());
+        if metrics_output_path.is_none() {
+            metrics_output_path = Some(format!("{}_metrics", output_path.unwrap()));
+        }
     } else {
         info!("Output view to stdout (first 10 items)");
     }

protocol-rpc/src/rpc/private-id/client.rs
@@ -7,6 +7,7 @@ use clap::App;
 use clap::Arg;
 use clap::ArgGroup;
 use common::gcs_path::GCSPath;
+use common::metrics;
 use common::s3_path::S3Path;
 use common::timer;
 use crypto::prelude::TPayload;
@@ -52,6 +53,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .short("o")
                 .takes_value(true)
                 .help("Path to output file, output format: private-id, option(key)"),
+            Arg::with_name("metric-path")
+                .long("metric-path")
+                .takes_value(true)
+                .help("Path to metric output file"),
             Arg::with_name("stdout")
                 .long("stdout")
                 .short("u")
@@ -150,6 +155,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let na_val = matches.value_of("not-matched-value");
     let use_row_numbers = matches.is_present("use-row-numbers");
 
+    let metric_path = matches.value_of("metric-path");
+    let metrics = metrics::Metrics::new("private-id-multi-key".to_string());
+    let mut metrics_output_path: Option<String> = None;
+    if let Some(val) = metric_path {
+        metrics_output_path = Some(val.to_string());
+    }
+
     let mut client_context = {
         let no_tls = matches.is_present("no-tls");
         let host_pre = matches.value_of("company");
@@ -177,6 +189,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("Input path: {}", input_path);
     if output_path.is_some() {
         info!("Output path: {}", output_path.unwrap());
+        if metrics_output_path.is_none() {
+            metrics_output_path = Some(format!("{}_metrics", output_path.unwrap()));
+        }
     } else {
         info!("Output view to stdout (first 10 items)");
     }
@@ -192,6 +207,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
     partner_protocol.gen_permute_pattern().unwrap();
     let u_partner = partner_protocol.permute_hash_to_bytes().unwrap();
+    metrics.set_partner_input_size(partner_protocol.get_size());
 
     // 5. Initialize company - this loads company's data and generates its permutation pattern
     let init_ack = match client_context
@@ -219,6 +235,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 7. Permute and encrypt data from company with own keys
     let (e_company, v_company) = partner_protocol.encrypt_permute(u_company);
+    metrics.set_publisher_input_size(e_company.len());
 
     // 8. Send partner's data to company
     let ack_u_partner =
@@ -324,6 +341,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 15. Create partner's ID spine and print
     partner_protocol.create_id_map(v_partner, s_prime_company, na_val);
+    metrics.set_union_file_size(partner_protocol.get_id_map_size());
     match output_path {
         Some(p) => {
             if let Ok(output_path_s3) = S3Path::from_str(p) {
@@ -355,6 +373,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
         None => partner_protocol.print_id_map(10, input_with_headers, use_row_numbers),
+    }
+    match &metrics_output_path {
+        Some(p) => {
+            if let Ok(metrics_path_s3) = S3Path::from_str(p) {
+                let s3_tempfile = tempfile::NamedTempFile::new().unwrap();
+                let (_file, path) = s3_tempfile.keep().unwrap();
+                let path = path.to_str().expect("Failed to convert path to str");
+                metrics
+                    .save_metrics(&String::from(path))
+                    .expect("Failed to write metrics to tempfile");
+                metrics_path_s3
+                    .copy_from_local(&path)
+                    .await
+                    .expect("Failed to write to S3");
+            } else {
+                metrics
+                    .save_metrics(p)
+                    .expect("Failed to write to metrics path");
+            }
+        }
+        None => {
+            metrics.print_metrics();
+        }
     }
 
     // 16. Create company's ID spine and print

protocol/src/private_id/partner.rs
@@ -217,4 +217,8 @@ impl PartnerPrivateIdProtocol for PartnerPrivateId {
             .map_err(|_| {});
         id_map_str.unwrap()
     }
+
+    fn get_id_map_size(&self) -> usize {
+        self.id_map.read().unwrap().len()
+    }
 }

protocol/src/private_id/traits.rs
@@ -21,6 +21,7 @@ pub trait PartnerPrivateIdProtocol {
         use_row_numbers: bool,
     ) -> Result<(), ProtocolError>;
     fn stringify_id_map(&self, use_row_numbers: bool) -> String;
+    fn get_id_map_size(&self) -> usize;
 }
 
 pub trait CompanyPrivateIdProtocol {

protocol/src/private_id_multi_key/partner.rs
@@ -253,6 +253,13 @@ impl PartnerPrivateIdMultiKeyProtocol for PartnerPrivateIdMultiKey {
             )),
         }
     }
+
+    fn get_id_map_size(&self) -> usize {
+        match self.id_map.clone().read() {
+            Ok(id_map) => id_map.len(),
+            _ => panic!("Cannot get id_map size"),
+        }
+    }
 }
 
 #[cfg(test)]

protocol/src/private_id_multi_key/traits.rs
@@ -16,6 +16,7 @@ pub trait PartnerPrivateIdMultiKeyProtocol {
     fn create_id_map(&self, partner: TPayload, company: TPayload);
     fn print_id_map(&self);
     fn save_id_map(&self, path: &str) -> Result<(), ProtocolError>;
+    fn get_id_map_size(&self) -> usize;
 }
 
 #[cfg_attr(test, automock)]